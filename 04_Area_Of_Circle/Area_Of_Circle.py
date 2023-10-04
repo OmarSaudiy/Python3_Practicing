c6ac31e9388c0fa5a816e6a3d8543818c620ad03
Omar Ahmed Hassan <omarsaudiy@gmail.com>
2023-10-04 16:20:08 +0300

    Area  of Circle

---
+++ 04_Area_Of_Circle/Area_Of_Circle.py
@@ -0,0 +1,12 @@
+# Author: Omar Ahmed Hassan                         #
+#                                                   #
+# Date  : 4 Oct, 2023                               #
+#                                                   #
+# Description : 04_Area_Of_Circle       			#
+
+#!/usr/bin/python3
+
+import math
+x = int(input("Enter Circle Radius in cm :"))
+
+print(f"Area of Circle ={x*x*math.pi} cm square")
