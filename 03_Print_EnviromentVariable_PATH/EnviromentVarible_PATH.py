19948c014436e116539b34738dda1a9575bde0dc
Omar Ahmed Hassan <omarsaudiy@gmail.com>
2023-10-04 16:12:25 +0300

    Print all Paths for Enviroment Variables

---
+++ 03_Print_EnviromentVariable_PATH/EnviromentVarible_PATH.py
@@ -0,0 +1,11 @@
+# Author: Omar Ahmed Hassan                         #
+#                                                   #
+# Date  : 4 Oct, 2023                               #
+#                                                   #
+# Description : 03_Print_Enviroment_Variables       #
+
+#!/usr/bin/python3
+
+import os
+
+print(os.environ["PATH"])
