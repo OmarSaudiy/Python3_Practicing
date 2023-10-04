fb1057e88c7eaf7c2e526ebb36fa311e08c226e5
Omar Ahmed Hassan <omarsaudiy@gmail.com>
2023-10-04 14:12:59 +0300

    Check Vowel Letter

---
+++ 02_Check_Vowel_Letter/Check_Vowel_Letter.py
@@ -0,0 +1,14 @@
+# Author: Omar Ahmed Hassan             #
+#                                       #
+# Date  : 4 Oct, 2023                   #
+#                                       #
+# Description : 01_Count_4s_in_list     #
+
+#!/usr/bin/python3
+
+x = input("Enter single English Alphabet : ")
+Vowels = "aiouAIOU"
+if x in Vowels :
+    print(f"{x} is Vowel")
+else:
+    print(f"{x} is Consonant")
