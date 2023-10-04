03ce85396d90217db2056164bc88484343481810
Omar Ahmed Hassan <omarsaudiy@gmail.com>
2023-10-04 16:34:42 +0300

    Calender Of Given Month and Year

---
+++ 05_Calender_Of_Given_M_Y/Calender_Of_Given_M_Y.py
@@ -0,0 +1,13 @@
+# Author: Omar Ahmed Hassan                         #
+#                                                   #
+# Date  : 4 Oct, 2023                               #
+#                                                   #
+# Description : 03_Print_Enviroment_Variables       #
+
+#!/usr/bin/python3
+
+import calendar
+x = int(input("Enter year "))
+y = int(input("Enter Month Number "))
+
+print(calendar.month(x,y))
