4731781adff837717d7f4fdb143c852e8de239a8
Omar Ahmed Hassan <omarsaudiy@gmail.com>
2023-10-04 13:55:13 +0300

    C and Python style for number of occurance

---
+++ 01_Count_4s_in_list/PyStyle_Count_4s_in_list.py
@@ -0,0 +1,12 @@
+# Author: Omar Ahmed Hassan             #
+#                                       #
+# Date  : 4 Oct, 2023                   #
+#                                       #
+# Description : 01_Count_4s_in_list     #
+
+#!/usr/bin/python3
+
+x = list(input("Enter List of a single digit elements:"))
+x = x.count('4')
+
+print(f"4 is found :{x} times ")
